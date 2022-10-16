78e588c2a11e02c7e51a267608ca7309afeeea8c
Sergey <motor-person@yandex.ru>
2022-10-16 23:30:41 +0400

    Дополнили Task_39 методом переворачивания массива через for

--- Task_39/Program.cs
+++ Task_39/Program.cs
@@ -32,25 +32,64 @@ int maximal = Convert.ToInt32(Console.ReadLine());
 int[] arr = CreateArrayRndInt(lengthArr, minimal, maximal);
 PrintArray(arr);
 System.Console.WriteLine();
+
+
+//________________________________________________________________________________________
+
+
 // Array.Reverse(arr);
 // PrintArray(arr);
+
+
+//________________________________________________________________________________________
+
 System.Console.WriteLine();
 
-void ReverseArray(int[] array)
+// void ReverseArray(int[] array)
+// {
+//     int size = array.Length;
+//     int i1 = 0;
+//     int i2 = size - 1;
+//     while (i1 < i2)
+//     {
+//         int obj = array[i1];
+//         array[i1] = array[i2];
+//         array[i2] = obj;
+
+//         i1++;
+//         i2--;
+//     }
+// }
+
+
+
+
+//________________________________________________________________________________________
+
+
+
+
+void ReverseArray2(int[] array)
 {
     int size = array.Length;
-    int i1 = 0;
-    int i2 = size - 1;
-    while (i1 < i2)
-    {
-        int obj = array[i1];
-        array[i1] = array[i2];
-        array[i2] = obj;
 
-        i1++;
-        i2--;
+    for (int i = 0; i < size / 2; i++)
+    {
+        Swap(array, i, array.Length - 1 - i);
     }
 }
 
-ReverseArray(arr);
+void Swap(int[] array, int i1, int i2)
+{
+    int temp = array[i1];
+    array[i1] = array[i2];
+    array[i2] = temp;
+}
+
+
+
+//________________________________________________________________________________________
+
+
+ReverseArray2(arr);
 PrintArray(arr);
