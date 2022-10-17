8ad5ad77b29f17c77358399adcb9bd97a3def600
Sergey <motor-person@yandex.ru>
2022-10-17 09:39:48 +0400

    Решили Task_42 преобразование из десятичного числа в двоичное

---
+++ Task_42/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.WriteLine("Пожалуйста, введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ConvertBinary(int num)
+{
+    int binaryNum = 0;
+    int count = 1;
+    while (num > 0)
+    {
+        binaryNum = binaryNum + num % 2 * count;
+        count = count * 10;
+        num = num / 2;
+    }
+    return binaryNum;
+}
+
+int binaryNumber = ConvertBinary(number);
+
+System.Console.WriteLine($"{number} -> {binaryNumber}");
