622e15266a49b79bb7b613479f65a040335722da
Sergey <motor-person@yandex.ru>
2022-10-17 09:02:55 +0400

    Решили Task_40

---
+++ Task_39/Task_40/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Пожалуйста, введите стороны треугольника");
+int sideA = Convert.ToInt32(Console.ReadLine());
+int sideB = Convert.ToInt32(Console.ReadLine());
+int sideC = Convert.ToInt32(Console.ReadLine());
+
+if (CanTraing(sideA, sideB, sideC)) Console.WriteLine($"Да, существует");
+else System.Console.WriteLine($"Нет, не существует");
+
+bool CanTraing(int side1, int side2, int side3)
+{
+    return side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2;
+}
